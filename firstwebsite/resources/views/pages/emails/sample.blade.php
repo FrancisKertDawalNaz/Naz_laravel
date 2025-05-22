81d59025d0f81da932cab2dbbd9f7fee8ad53b13
kert <nfranciskert@gmail.com>
2025-05-22 12:30:04 +0800

    updated

--- firstwebsite/resources/views/pages/emails/sample.blade.php
+++ firstwebsite/resources/views/pages/emails/sample.blade.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>{{ $details['subject'] ?? 'Sample Email' }}</title>
-</head>
-<body>
-    <h2>{{ $details['subject'] ?? 'Sample Email' }}</h2>
-    <p>{{ $details['body'] ?? 'This is a sample email content.' }}</p>
-</body>
-</html>
+@include('partials.header')
+@include('partials.nav')
+<div class="container py-5">
+    <div class="card shadow mx-auto" style="max-width: 600px;">
+        <div class="card-body">
+            <h2 class="card-title text-primary">{{ $details['subject'] ?? 'Sample Email' }}</h2>
+            <p class="card-text">{{ $details['body'] ?? 'This is a sample email content.' }}</p>
+        </div>
+    </div>
+</div>
